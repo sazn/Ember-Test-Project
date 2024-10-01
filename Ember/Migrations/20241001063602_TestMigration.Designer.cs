485a5ccaf5d7c85ab9e4c2b177c07cf6042ad605
sazn <sazn_004@hotmail.com>
2024-10-01 16:24:36 +0545

    push to origin branch

--- Ember/Migrations/20241001063602_TestMigration.Designer.cs
+++ Ember/Migrations/20241001063602_TestMigration.Designer.cs
@@ -4,6 +4,7 @@ using Ember.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ember.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20241001063602_TestMigration")]
+    partial class TestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Ember/Models/Registration.cs
+++ Ember/Models/Registration.cs
@@ -3,10 +3,10 @@
     public class Registration
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string AccountType { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? AccountType { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
